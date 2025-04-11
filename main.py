4ed43262a488abdc1b5cae3b9cf8a0f1e3da9d5f
ArtemisNyx3 <artemisnyx3@yahoo.com>
2025-04-11 00:22:31 -0400

    detectCollision initial

--- main.py
+++ main.py
@@ -28,15 +28,21 @@ def main():
     asteroidField = AsteroidField()
     
     # Game Loop
-    while(True):
+    running = True
+    while(running):
         screen.fill("black")
         updatable.update(dt)
+        for obj in asteroids:
+            if obj.detectCollision(player):
+                print("Game Over!!!")
+                running = False
+                break
         for obj in drawable:
             obj.draw(screen)
         pygame.event.pump()
         pygame.display.flip() # Refresh Screen
         dt = clock.tick(60)/1000
-    
+     
 
 if __name__ == "__main__":
     main()
