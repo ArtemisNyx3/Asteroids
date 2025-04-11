4dc6bdea4640311c24c00f9d0716398827e552fb
ArtemisNyx3 <artemisnyx3@yahoo.com>
2025-04-10 22:50:31 -0400

    player sprite  drawn (CH2 L5)

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
+from constants import *
 import pygame
+from player import Player
 
 def main():
     print("Starting Asteroids!")
+    pygame.init()
+    print(f"Screen width: {SCREEN_WIDTH}") 
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock() # Game Clock 
+    dt = 0 # delta time 
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2) 
+    # Game Loop
+    while(True):
+        screen.fill("black")
+        player.draw(screen)
+        pygame.display.flip() # Refresh Screen
+        dt = clock.tick(60)/1000
+        pass
+    
 
 if __name__ == "__main__":
     main()
